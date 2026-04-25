d3d17415227d449e1dc58082b669f1719ae6661b
mehrab-abir <mehrababir100@gmail.com>
2026-04-25 01:45:19 -0400

    audio again

--- Streamlit/audio.py
+++ Streamlit/audio.py
@@ -4,3 +4,9 @@ st.title(":blue[Audio Uploader:]",anchor=False)
 st.divider()
 
 st.audio("audios/game_of_thrones.mp3",loop=True)
+
+audio = st.file_uploader("Upload audio: ",
+                         type=["mp3","aac"])
+
+if(audio):
+    st.audio(audio)
