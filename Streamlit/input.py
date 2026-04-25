9f6f244db6e4936f328302dfda262b00539f5e71
mehrab-abir <mehrababir100@gmail.com>
2026-04-25 00:29:58 -0400

    select box

--- Streamlit/input.py
+++ Streamlit/input.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("User input:")
+st.title("User input:",anchor=False)
 st.divider()
 
 name = st.text_input("Enter Name:",placeholder="Your Name...")
@@ -15,7 +15,21 @@ if(pressed):
     
 st.divider()
 
-st.header("Add two numbers: ")
+st.header(":green[Selection Box:]",anchor=False)
+selected = st.selectbox("Your Profession: ",
+                        ("Student","Engineer","Doctor","Govt. Employee"),
+                        index=None,
+                        accept_new_options=True
+                    )
+# index=None --> no value will be selected by default
+# accept_new_options=True --> let user enter other options
+if selected:
+    st.write("Profession Selected: ",selected)
+    
+    
+st.divider()
+
+st.header("Add two numbers: ",anchor=False)
 
 num1 = st.number_input("Number 1: ",value=None)
 num2 = st.number_input("Number 2: ",value=None)
@@ -24,3 +38,4 @@ addBtn = st.button("Add",type="secondary")
 
 if addBtn:
     st.write(f"Result: {num1+num2}")
+    
